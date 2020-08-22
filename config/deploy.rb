d9d792960470daa7db21546539893a985d6a1660
Dinis <dinis@lage.pw>
2020-08-22 12:41:27 +0100

    Add .env loading to capistrano deployment.

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,3 +41,7 @@ set :keep_releases, 5
 set :keep_assets, 5
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+namespace :deploy do
+  after  :starting, 'envvars:load'
+end
